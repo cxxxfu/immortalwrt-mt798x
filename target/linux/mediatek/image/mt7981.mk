e2c7e17c1142f6e9da9d7af03c72181ca73312a2
cxxxfu <33364582+cxxxfu@users.noreply.github.com>
2024-12-04 15:38:52 +0800

    Update mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -14,6 +14,18 @@ define Device/mt7981-spim-nor-rfb
 endef
 TARGET_DEVICES += mt7981-spim-nor-rfb
 
+
+define Device/mt7981-super3000m
+  DEVICE_VENDOR := cxxxfu
+  DEVICE_MODEL := mt7981-super3000m
+  DEVICE_DTS := mt7981-super3000m
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  DEVICE_PACKAGES := $(MT7981_USB_PKGS) f2fsck losetup mkf2fs kmod-fs-f2fs kmod-mmc \
+	luci-app-samba4
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += mt7981-super3000m
+
 define Device/mt7981-spim-nand-2500wan-gmac2
   DEVICE_VENDOR := MediaTek
   DEVICE_MODEL := mt7981-spim-nand-2500wan-gmac2
